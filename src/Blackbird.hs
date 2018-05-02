aa468635a362d2b49186ef6ba37579e4aedd4f6c
Ryan Moore <paniclater@gmail.com>
2018-05-02 18:52:05 +0200

    moving blackbird biz to its own files

---
+++ src/Blackbird.hs
@@ -0,0 +1,10 @@
+module Blackbird where
+
+import Nand
+import Voltage
+import CombinatoryBoolean
+import Data.Aviary.Birds
+(...) = blackbird
+
+and'' :: Voltage -> Voltage -> Voltage
+and'' = undefined
