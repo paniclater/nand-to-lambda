3b0176b4a7dff86005a58198508fe269ebf207c1
Ryan Moore <paniclater@gmail.com>
2018-05-02 18:52:24 +0200

    getting rid of unused combi boolean biz

--- src/CombinatoryBoolean.hs
+++ src/CombinatoryBoolean.hs
@@ -2,25 +2,9 @@ module CombinatoryBoolean where
 
 import Nand
 import Voltage
-import Data.Function
-import Data.Aviary.Birds
-(...) = blackbird
 
 not' :: Voltage -> Voltage
-not' x = nand x x
+not' = undefined
 
 and' :: Voltage -> Voltage -> Voltage
-and' x y = not' (nand x y)
-
-and'' :: Voltage -> Voltage -> Voltage
-and'' = not' ... nand
-
-or' :: Voltage -> Voltage -> Voltage
-or' Low Low = Low
-or' _ _ = High
-
-xor :: Voltage -> Voltage -> Voltage
-xor High High = Low
-xor Low  Low = Low
-xor _ _ = High
-
+and' = undefined
