bce41ed526e5cc469c59a18d8cc50d98cf515625
Ryan Moore <paniclater@gmail.com>
2018-05-02 19:27:53 +0200

    unimplementing full and half adder

--- src/FullAdder.hs
+++ src/FullAdder.hs
@@ -6,9 +6,4 @@ import Voltage
 import VoltageOr
 
 fullAdder :: Voltage -> Voltage -> Voltage -> (Voltage, Voltage)
-fullAdder x y z =
-  (sum, carry)
-      where
-        (sum1, carry1) = halfAdder x y
-        (sum, carry2) = halfAdder sum1 z
-        Or carry = Or carry1 <> Or carry2
+fullAdder = undefined
--- src/HalfAdder.hs
+++ src/HalfAdder.hs
@@ -6,7 +6,4 @@ import VoltageAnd
 import VoltageXor
 
 halfAdder :: Voltage -> Voltage -> (Voltage, Voltage)
-halfAdder x y = (sum, carry)
-  where
-    Xor sum = Xor x <> Xor y
-    And carry = And x <> And y
+halfAdder = undefined
